c54f0c2c7810762c01930ac1077dd6cceac3f76c
Kai <1114366961@qq.com>
2021-03-31 15:18:14 +0800

    deploy

---
+++ precache-manifest.97bd06f04e088617f2ce3a12c6b461cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5595a6cddb053d2d91f",
+    "url": "/Riches-Vue-CLI/css/app.7177eeb7.css"
+  },
+  {
+    "revision": "f69fc0764863919f01fc1738e68d9dac",
+    "url": "/Riches-Vue-CLI/img/logo.f69fc076.png"
+  },
+  {
+    "revision": "d0910495159153588216ee85b721d482",
+    "url": "/Riches-Vue-CLI/index.html"
+  },
+  {
+    "revision": "e5595a6cddb053d2d91f",
+    "url": "/Riches-Vue-CLI/js/app.5978a982.js"
+  },
+  {
+    "revision": "61490065ed8f657423cd",
+    "url": "/Riches-Vue-CLI/js/chunk-vendors.403040e6.js"
+  },
+  {
+    "revision": "cd12d8940b67eb1a0c54e68abf1d17e7",
+    "url": "/Riches-Vue-CLI/logo.png"
+  },
+  {
+    "revision": "401a5a8033be9cacf1ede18edfe6dc88",
+    "url": "/Riches-Vue-CLI/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Riches-Vue-CLI/robots.txt"
+  }
+]);
